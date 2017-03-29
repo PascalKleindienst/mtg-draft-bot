b80e3a9be0a3473e446ba8c1a29f9db7c82d941b
PascalKleindienst <mail@pascalkleindienst.de>
2017-03-29 15:13:34 +0200

    added Strategy class + test

---
+++ Memnarch/Strategy/Strategy.py
@@ -0,0 +1,23 @@
+from abc import ABCMeta
+
+
+class Strategy:
+    __metaclass__ = ABCMeta
+
+    @classmethod
+    def analyze_content(cls, text, kwic):
+        """
+        Analyze card text for keywords
+        :param text: string
+        :param kwic: list
+        :return: int
+        """
+        text = text.lower().split()
+        score = 0
+
+        for token in text:
+            for kw in kwic:
+                if kw == token:
+                    score = score + 1
+
+        return score
---
+++ tests/test_strategy/test_strategy.py
@@ -0,0 +1,17 @@
+from nose.tools import assert_equal
+
+from Memnarch.Strategy.Strategy import Strategy
+
+
+class TestStrategy(object):
+    @classmethod
+    def setup_class(cls):
+        """This method is run once for each class before any tests are run"""
+        cls.strategy = Strategy()
+
+    @classmethod
+    def teardown_class(cls):
+        """This method is run once for each class _after_ all tests are run"""
+
+    def test_analyze_content(self):
+        assert_equal(4, self.strategy.analyze_content('Lorem ipsum dolor sit amet. Lorem ipsum', ('lorem', 'ipsum')))
